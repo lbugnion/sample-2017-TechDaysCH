e0724c991636414376915bae3c91ce3290fa2dfe
Laurent Bugnion <laurent@galasoft.ch>
2017-02-03 09:14:16 +0100

    First commit with demos for Centennial

---
+++ TileHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace Helpers
+{
+    public static class TileHelper
+    {
+        public static void ShowTileNotification(string message)
+        {
+            var nowTimeString = DateTime.Now.ToString(CultureInfo.CurrentCulture);
+
+            var xml = $@"
+                <tile version='3'>
+                    <visual branding='nameAndLogo'>
+
+                        <binding template='TileMedium'>
+                            <text hint-wrap='true'>{message}</text>
+                            <text hint-wrap='true' hint-style='captionSubtle'>{nowTimeString}</text>
+                        </binding>
+
+                        <binding template='TileWide'>
+                            <text hint-wrap='true'>{message}</text>
+                            <text hint-wrap='true' hint-style='captionSubtle'>{nowTimeString}</text>
+                        </binding>
+
+                        <binding template='TileLarge'>
+                            <text hint-wrap='true'>{message}</text>
+                            <text hint-wrap='true' hint-style='captionSubtle'>{nowTimeString}</text>
+                        </binding>
+
+                </visual>
+            </tile>";
+
+            var doc = new XmlDocument();
+            doc.LoadXml(xml);
+
+            var notification = new TileNotification(doc);
+            TileUpdateManager.CreateTileUpdaterForApplication().Update(notification);
+        }
+    }
+}
---
+++ WpfDemo/WpfDemo/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+using Helpers;
+
+namespace WpfDemo
+{
+    public partial class MainWindow : Window
+    {
+        private Storyboard _sbd;
+        private int _counter;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitClick(object sender, RoutedEventArgs e)
+        {
+            if (_sbd == null)
+            {
+                _sbd = (Storyboard)Resources["BallStoryboard"];
+            }
+
+            _sbd.Stop();
+            _sbd.Begin();
+
+            var message = $"Submit clicked {++_counter} times";
+            TileHelper.ShowTileNotification(message);
+            ToastHelper.PopToast("TechDays!!", message);
+        }
+    }
+}
